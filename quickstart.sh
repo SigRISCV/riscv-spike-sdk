10fb8f25755006b17d41f7b64a1bb2def887dd5b
zyy <1076192792@QQ.com>
2025-11-19 19:28:59 +0800

    change quickstart.sh

--- quickstart.sh
+++ quickstart.sh
@@ -32,11 +32,9 @@ cd $root
 root="$(dirname "$(readlink -f "$0")")"
 NJOB=4
 
-root_repo_list=${ROOT_LIST:-"buildroot riscv-gnu-toolchain riscv-pk linux riscv-isa-sim"}
-toolchain_repo_list=${TOOLCHAIN_LIST:-"binutils gcc glibc newlib gdb"}
+root_repo_list=${ROOT_LIST:-"freebsd llvm-project opensbi qemu"}
 
 fetch_repo "$root/repo" "$root_repo_list" "--jobs $NJOB"
-fetch_repo "$root/repo/riscv-gnu-toolchain" "$toolchain_repo_list" "--recursive --jobs $NJOB"
 
 echo "[*] done"
 
